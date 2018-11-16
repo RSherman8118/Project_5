96890f020d3088e3ceae1e44112cf07db4abfe5c
Rebecca <shermarl@newschool.edu>
2018-11-16 08:27:53 -0500

    photos

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-function myFunction() {
-    myimg = document.getElementById("myImg");
-    myimg.src = "../Project_5/kid1.jpg";
+myimg = document.getElementById("myImg");
 
-    myimg.classList.toggle("animated");
+// function myFunction() {
+//     myimg.src = "../Project_5/kid1.jpg";
+
+//     myimg.classList.toggle("animated");
+
+//     myimg2 = document.getElementById("myimg2")
 
 
-    //.className ='image1';
 
-}
+
+
+
+// }
+
+myimg.addEventListener("mouseover", function(){
+	myimg.src = "../Project_5/kid5.jpg";
+	myimg.classList.add("animated")
+})
+
+
+
+
+myimg2 = document.getElementById("myImg2");
+
+
+
+myimg2.addEventListener("mouseover", function(){
+	myimg2.src = "../Project_5/kid2.jpg";
+	myimg2.classList.add("animated2")
+})
+
+
+
+myimg3 = document.getElementById("myImg3");
+
+
+
+myimg3.addEventListener("mouseover", function(){
+	myimg3.src = "../Project_5/kid3.jpg";
+	myimg3.classList.add("animated3")
+})
+
+
+
+myimg4 = document.getElementById("myImg4");
+
+
+
+myimg4.addEventListener("mouseover", function(){
+	myimg4.src = "../Project_5/kid4.jpg";
+	myimg4.classList.add("animated4")
+})
 
 
 
